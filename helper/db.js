3f4f29d6dac8257ea63f69dce48a20aad955950c
Fatih <fatih47154@gmail.com>
2019-03-15 02:11:55 +0300

    Post: /api/movies - Models: Movie - DB Connection

---
+++ helper/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://fatih47154:Fatih4715434_@ds155294.mlab.com:55294/heroku_rv295r8q', { useNewUrlParser: true });
+    
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB: Connected');
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB: Error', err);
+    });
+
+    mongoose.Promise = global.Promise;
+};
