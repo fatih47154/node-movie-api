2ef658863abcd09e17f50c1765d753a161e0a65b
Fatih <fatih47154@gmail.com>
2019-03-16 01:34:23 +0300

    Get: /api/movies - Get: /api/movies/:movie_id

--- routes/movie.js
+++ routes/movie.js
@@ -4,6 +4,27 @@ const router = express.Router();
 // Models
 const Movie = require('../models/Movie');
 
+router.get('/', (req, res, next) => {
+  const promise = Movie.find({ });
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.get('/:movie_id', (req, res, next) => {
+  const promise = Movie.findById(req.param.movie_id);
+  promise.then((movie) => {
+    if(!movie)
+      next({ message: 'The movie was not found.', code: 99 });
+
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
 router.post('/', (req, res, next) => {
   // const { title, imdb_score, category, country, year } = req.body;
   
