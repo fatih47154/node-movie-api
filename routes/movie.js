10d317ce480294467e09be78f226b83fb7dbc4f5
Fatih <fatih47154@gmail.com>
2019-03-16 20:45:30 +0300

    Get: /movies/top10, /between/start_year/end_year, Delete: /movies/{id}, Put: /movies/{id}

--- routes/movie.js
+++ routes/movie.js
@@ -4,8 +4,10 @@ const router = express.Router();
 // Models
 const Movie = require('../models/Movie');
 
+// Get: ../api/movies
 router.get('/', (req, res, next) => {
   const promise = Movie.find({ });
+
   promise.then((data) => {
     res.json(data);
   }).catch((err) => {
@@ -13,8 +15,21 @@ router.get('/', (req, res, next) => {
   });
 });
 
+// Get: ../api/top10
+router.get('/top10', (req, res, next) => {
+  const promise = Movie.find({ }).limit(10).sort({ imdb_score: -1 });
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+}); 
+
+// Get: ../api/movies/{id}
 router.get('/:movie_id', (req, res, next) => {
-  const promise = Movie.findById(req.param.movie_id);
+  const promise = Movie.findById(req.params.movie_id);
+
   promise.then((movie) => {
     if(!movie)
       next({ message: 'The movie was not found.', code: 99 });
@@ -25,14 +40,50 @@ router.get('/:movie_id', (req, res, next) => {
   });
 });
 
+// Put: ../api/movies/{id}
+router.put('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(
+      req.params.movie_id,
+      req.body,
+      { new: true } // Eğer kayıttan sonra çıktıyı ekranda görmek istersek
+    );
+
+  promise.then((movie) => {
+    if(!movie)
+      next({ message: 'The movie was not found.', code: 99 });
+
+    res.json(movie);
+  //res.json({ status: 1 }); 
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+// Delete: ../api/movies/{id}
+router.delete('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndDelete(req.params.movie_id);
+
+  promise.then((movie) => {
+    if(!movie)
+      next({ message: 'The movie was not found.', code: 99 });
+
+  //res.json(movie);
+    res.json({ status: 1 });
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+// Post: ../api/movies  [From Body]
 router.post('/', (req, res, next) => {
   // const { title, imdb_score, category, country, year } = req.body;
   
   const movie = new Movie(req.body);
   const promise = movie.save();
 
-  promise.then(() => {
-    res.json({ status: 1 });
+  promise.then((data) => {
+  //res.json({ status: 1 });
+    res.json(data)
   }).catch((err) => {
     res.json(err);
   });
@@ -44,6 +95,26 @@ router.post('/', (req, res, next) => {
   //   res.json({ status: 1 });
   // });
 
+});
+
+// Get: ../api/movies/between   //    $gte: greaterThenEqual
+                                //    $lte: lessThenEqual
+                                //    $gt:  greaterThen
+                                //    $lt:  lessThen
+router.get('/between/:start_year/:end_year', (req, res, next) => {
+  const { start_year, end_year } = req.params;
+  const promise = Movie.find(
+    { 
+      year: { "$gte": parseInt(start_year), "$lte": parseInt(end_year) }
+    //year: { "$gt": parseInt(start_year), "$lt": parseInt(end_year) }  
+    }
+  );
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
