1c7e1ba86d955140ee7d4e48cb12694b8c241acf
Fatih <fatih47154@gmail.com>
2019-03-16 22:58:24 +0300

    Movie Model Validation

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,26 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        require: true
+        required: [true, '`{PATH}` Alanı Zorunludur.'],
+        maxlength: [15, '`{PATH}` Alanı (`{VALUE}`), `{MAXLENGTH}` Karakterden Fazla Olamaz.'],
+        minlength: [3, '`{PATH}` Alanı (`{VALUE}`), `{MINLENGTH}` Karakterden Az Olamaz.']
     },
-    category: String,
+    category: {
+        type: String,
+        maxlength: [15, '`{PATH}` Alanı (`{VALUE}`), `{MAXLENGTH}` Karakterden Fazla Olamaz.'],
+        minlength: [3, '`{PATH}` Alanı (`{VALUE}`), `{MINLENGTH}` Karakterden Az Olamaz.']
+    },
     country: String,
-    year: Number,
-    imdb_score: Number,
+    year: {
+        type: Number,
+        max: 2040,
+        min: 1900
+    },
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now
