8f98da4a533b0e87b1fd69a385697ee563d8f358
Fatih <fatih47154@gmail.com>
2019-03-16 21:02:51 +0300

    Post: api/directors/

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 
 const indexRouter = require('./routes/index');
 const movie = require('./routes/movie.js');
+const director = require('./routes/director');
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/movies', movie);
+app.use('/api/directors', director);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
