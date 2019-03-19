f68e6503fe7d8fc259e0aa1180fb53590d8f6651
Fatih <fatih47154@gmail.com>
2019-03-20 00:53:11 +0300

    /Put/:movie_id Movie Test

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -81,4 +81,33 @@ describe('Movie /api/movies Test', () => {
                 });
         });
     });
+
+    describe('/Put/:movie_id Movie', () => {
+        it('It Should Update a Movie Given by ID', (done) => {
+            const movie = {
+                title: '93creative',
+                director_id: '5c8d3bb7cb235224e01b791c',
+                category: 'Crime',
+                country: 'France',
+                year: 1970,
+                imdb_score: 9
+            };
+
+            chai.request(server)
+                .put('/api/movies/' + movieId)
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category);
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+                    done();
+                });
+        })
+    });
 });
